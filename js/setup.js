12ab7edbfa84a7f2efb2e2e8ef4259d45fa4c89f
Marat765 <marat76593@gmail.com>
2018-07-08 21:39:04 +0300

    Мелкие правки

--- js/setup.js
+++ js/setup.js
@@ -10,10 +10,10 @@ var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
-var i;
 var wizards = [];
 
 function getArray() {
+  var i;
   for (i = 0; i < NUMBER_OF_WIZARDS; i++) {
     var getRandomItem = function (arr) {
       return arr[Math.floor(Math.random() * arr.length)];
@@ -36,16 +36,15 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 
 function renderWizard(wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-
   return wizardElement;
 }
 
 function renderSimilarWizards() {
   var fragment = document.createDocumentFragment();
+  var i;
   for (i = 0; i < wizards.length; i++) {
     fragment.appendChild(renderWizard(wizards[i]));
   }
